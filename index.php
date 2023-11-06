6d6372fd7f272b9a3b775b2b5af07b692a1f5a7f
Melozzo <carmecamma1995@gmail.com>
2023-11-06 20:15:06 +0100

    [change]: photos

--- index.php
+++ index.php
@@ -37,17 +37,24 @@ $movies=[
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
   <title>Document</title>
 </head>
 <body>
   
 <?php foreach($movies as $movie): ?>
-  <ul>
-    <li><?php echo $movie->title ?></li>
-    <li><img src="./img/<?php echo $movie->image->image_name ?>" alt="<?php echo $movie->image->title_image ?>"></li>
-    <li><?php echo $movie->vote ?></li>
-    <li><?php echo implode("/", $movie->genre) ?></li>
-  </ul>
+  <div class="card" style="width: 18rem;">
+  <img src="./img/<?php echo $movie->image->image_name ?>" alt="<?php echo $movie->image->title_image ?>">
+  <div class="card-body">
+    <h5 class="card-title"><?php echo $movie->title ?></h5>
+    <p class="card-text"><?php echo $movie->vote ?></p>
+    <p class="card-text"><?php echo implode("/", $movie->genre) ?></p>
+  </div>
+</div>
   <?php endforeach; ?>
 </body>
 </html>
