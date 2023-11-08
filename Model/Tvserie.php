b54f65b617d663f3a46426fbc56a88c254836124
Melozzo <carmecamma1995@gmail.com>
2023-11-08 20:21:58 +0100

    [add&remove]: construct and remove movie file

--- Model/Tvserie.php
+++ Model/Tvserie.php
@@ -1,14 +1,14 @@
 <?php
 
-class Tvserie extends Movie{
-  public $duration_tvserie;
+class Tvserie extends Production{
+  public $season;
   public $production_year;
   
-  public function __construct (string $_title, Media $_image, float $_vote, array $_genre, int $_duration_tvserie, int $_production_year){
+  public function __construct (string $_title, Media $_image, float $_vote, array $_genre, int $_season, int $_production_year){
 
     $this->production_year = $_production_year;
-    $this->duration_tvserie = $_duration_tvserie;
+    $this->season = $_season;
 
-    parent::__construct (string $_title, Media $_image, float $_vote, array $_genre);
+    parent::__construct ($_title, $_image, $_vote, $_genre);
   }
 }
