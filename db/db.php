76f5a6f268fdec0038d19eb1b60faaacbcd76441
Melozzo <carmecamma1995@gmail.com>
2023-11-08 20:20:26 +0100

    [edit]: new product

--- db/db.php
+++ db/db.php
@@ -1,7 +1,11 @@
 <?php
 $films=[
-  new Film ('Una settimana da Dio', new Media ('cover.jpg', 'Una settimana da Dio'), 6, ['commedia', 'teatro']),
-  new Film ('Il gladiatore', new Media ('gladiatore.webp', 'Il gladiatore'), 9, ['storico', 'drammatico']),
-  new Film ('La madre', new Media ('acqua.jpg', 'La madre'), 3, ['thriller', 'sentimentale']),
-  new Film ('Doraemon', new Media ('doraemon.jpg', 'Doraemon'), 5, ['fantasia', 'bimbi'])
+  new Film ('Una settimana da Dio', new Media ('cover.jpg', 'Una settimana da Dio'), 6, ['commedia', 'teatro'], 110, 2000),
+  new Film ('Il gladiatore', new Media ('gladiatore.webp', 'Il gladiatore'), 9, ['storico', 'drammatico'], 110, 2000),
+  new Film ('La madre', new Media ('acqua.jpg', 'La madre'), 3, ['thriller', 'sentimentale'], 110, 2000),
+  new Film ('Doraemon', new Media ('doraemon.jpg', 'Doraemon'), 5, ['fantasia', 'bimbi'], 110, 2000),
+  
+  new Tvserie ('Doraemon', new Media ('doraemon.jpg', 'Doraemon'), 5, ['fantasia', 'bimbi'], 11, 2000),
+  new Tvserie ('Dragonball', new Media ('doraemon.jpg', 'Doraemon'), 5, ['fantasia', 'bimbi'], 11, 2000),
+  new Tvserie ('Ncis', new Media ('doraemon.jpg', 'Doraemon'), 5, ['fantasia', 'bimbi'], 11, 2000)
 ];
