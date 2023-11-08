73ac5eb6477c9fa3736991dabf43c32139aaed4f
Melozzo <carmecamma1995@gmail.com>
2023-11-08 17:27:49 +0100

    [edit]: embed files in index.php

--- index.php
+++ index.php
@@ -1,35 +1,8 @@
 <?php
-class Movie {
-  public $title;
-  public $image;
-  public $vote;
-  public $genre;
+require_once __DIR__ . "/Model/Media.php";
+require_once __DIR__ . "/Model/Movie.php";
+require_once __DIR__ . "/db/db.php";
 
-  public function __construct (string $_title, Media $_image, float $_vote, array $_genre){
-    $this->title = $_title;
-    $this->image = $_image;
-    $this->vote = $_vote;
-    $this->genre = $_genre;
-  }
-}
-
-class Media {
-
-  public $image_name;
-  public $title_image;
-
-  public function __construct(string $_image_name, string $_title_image){
-    $this->image_name = $_image_name;
-    $this->title_image = $_title_image;
-  }
-}
-
-$movies=[
-  new Movie ('Una settimana da Dio', new Media ('acqua.jpg', 'Una settimana da Dio'), 8, ['commedia', 'teatro']),
-  new Movie ('Una semana como Dios', new Media ('cover.jpg', 'Una settimana da Dio'), 7, ['commedia', 'musical']),
-  new Movie ('Like a Boss', new Media ('love.webp', 'Una settimana da Dio'), 10, ['commedia', 'sonetto']),
-  new Movie ('Funken Odin', new Media ('world.webp', 'Una settimana da Dio'), 5, ['commedia', 'thriller'])
-  ]
 ?>
 
 <!DOCTYPE html>
