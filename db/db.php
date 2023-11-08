079acbeab4a2b165c485dbea5f8af7f3722af1e9
Melozzo <carmecamma1995@gmail.com>
2023-11-08 17:26:29 +0100

    [fix]: array in db

--- db/db.php
+++ db/db.php
@@ -4,4 +4,4 @@ $movies=[
   new Movie ('Il gladiatore', new Media ('cover.jpg', 'Una settimana da Dio'), 9, ['storico', 'drammatico']),
   new Movie ('La madre', new Media ('love.webp', 'Una settimana da Dio'), 3, ['thriller', 'sentimentale']),
   new Movie ('Doraemon', new Media ('world.webp', 'Una settimana da Dio'), 5, ['fantasia', 'bimbi'])
-  ]
+];
