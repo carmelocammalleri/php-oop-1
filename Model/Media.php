49366ab83e9ed06d57e0657e8494e0d72084f290
Melozzo <carmecamma1995@gmail.com>
2023-11-08 17:20:06 +0100

    [add]: files db, movie and media

--- Model/Media.php
+++ Model/Media.php
@@ -1,1 +1,12 @@
 <?php
+
+class Media {
+
+  public $image_name;
+  public $title_image;
+
+  public function __construct(string $_image_name, string $_title_image){
+    $this->image_name = $_image_name;
+    $this->title_image = $_title_image;
+  }
+}
---
+++ db/db.php
@@ -0,0 +1,7 @@
+<?php
+$movies=[
+  new Movie ('Una settimana da Dio', new Media ('acqua.jpg', 'Una settimana da Dio'), 6, ['commedia', 'teatro']),
+  new Movie ('Il gladiatore', new Media ('cover.jpg', 'Una settimana da Dio'), 9, ['storico', 'drammatico']),
+  new Movie ('La madre', new Media ('love.webp', 'Una settimana da Dio'), 3, ['thriller', 'sentimentale']),
+  new Movie ('Doraemon', new Media ('world.webp', 'Una settimana da Dio'), 5, ['fantasia', 'bimbi'])
+  ]
